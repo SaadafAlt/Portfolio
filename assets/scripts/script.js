f5afdf5fdc35fb5c5c73d8e1d9e3f9040c777a0e
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-07-05 23:12:14 -0400

    creating copy to clipboard functionality for contact form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,44 @@
-const myEmail = document.getElementById("my-email");
+/**
+ * "Click to Copy" function
+ * Created in a generic way so that it can be used anywhere
+ 
+function copyText() {
+    const myEmail = document.getElementById("copy-email-wrapper");
+
+    myEmail.textContent = "Copied!";
+
+    setTimeout(() => {
+        myEmail.textContent = `<p id="my-email">saadafmohsin@gmail.com</p>`;
+    }, 1000);
+}
+    */ 
+/*
+function clickToCopy(htmlElement) {
+    if (!htmlElement) {
+        return;
+    }
+    
+    // we need to create a temp. input element 
+    let elementText = htmlElement.innerText;
+    let inputElement = document.createElement('input');
+    document.body.appendChild(inputElement);
+
+    inputElement.select(); // selects aka highlights 
+    document.execCommand('copy');
+    inputElement.parentNode.removeChild(inputElement);
+}
+
+// get div, attach onclick event handler to it then set it to an anon func.
+document.querySelector('#copy-email-wrapper').onclick = function () {
+    // pass in targeted div to function
+    clickToCopy(document.querySelector('#my-email'));
+}
+*/
+
+function copyText() {
+    const myEmail = document.getElementById("my-email").innerHTML;
+    const copyBtn = document.getElementById("copy-email-btn");
+    const newBtn = document.createElement("button");
+    newBtn.innerHTML = myEmail;
+}
+
