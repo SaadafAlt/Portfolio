5846e26405f3ea8b8f8f8953f3463cc52875b03f
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-07-06 14:15:00 -0400

    added 'copy to clipboard' functionality to email btn

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,44 +1,17 @@
-/**
- * "Click to Copy" function
- * Created in a generic way so that it can be used anywhere
- 
-function copyText() {
-    const myEmail = document.getElementById("copy-email-wrapper");
+// Write to the system clipboard aka copy.
+const emailElement = document.getElementById("my-email");
+const emailValue = emailElement.innerText;
+const copyContent = async () => {
+    try {
+        await navigator.clipboard.writeText(emailValue);
 
-    myEmail.textContent = "Copied!";
+        emailElement.innerText = "Copied!";
+        setTimeout(() => {
+            emailElement.innerText = "saadafmohsin@gmail.com";
+        }, 1000);
 
-    setTimeout(() => {
-        myEmail.textContent = `<p id="my-email">saadafmohsin@gmail.com</p>`;
-    }, 1000);
-}
-    */ 
-/*
-function clickToCopy(htmlElement) {
-    if (!htmlElement) {
-        return;
+        console.log('Content copied to Clipboard');
+    } catch (err) {
+        console.error('Failed to copy: ', err);
     }
-    
-    // we need to create a temp. input element 
-    let elementText = htmlElement.innerText;
-    let inputElement = document.createElement('input');
-    document.body.appendChild(inputElement);
-
-    inputElement.select(); // selects aka highlights 
-    document.execCommand('copy');
-    inputElement.parentNode.removeChild(inputElement);
 }
-
-// get div, attach onclick event handler to it then set it to an anon func.
-document.querySelector('#copy-email-wrapper').onclick = function () {
-    // pass in targeted div to function
-    clickToCopy(document.querySelector('#my-email'));
-}
-*/
-
-function copyText() {
-    const myEmail = document.getElementById("my-email").innerHTML;
-    const copyBtn = document.getElementById("copy-email-btn");
-    const newBtn = document.createElement("button");
-    newBtn.innerHTML = myEmail;
-}
-
