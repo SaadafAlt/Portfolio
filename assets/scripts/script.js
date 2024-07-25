639cd08cb169d92805b1cee2963dc48e4a03258a
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-07-24 22:05:19 -0400

    clear fields after user submits email

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -76,31 +76,12 @@ function sendEmail() {
   });
 }
 
-/*
-    function checkInputs() {
-        const items = document.querySelectorAll(".item");
-
-        for (const item of items) {
-            if (item.value == "") {
-                item.classList.add("error");
-                item.parentElement.classList.add("error");
-            }
-        }
-
-        item.addEventListener("keyup", () => {
-            if (item.value != "") {
-                item.classList.remove("error");
-                item.parentElement.classList.remove("error");
-            } else {
-                item.classList.add("error");
-                item.parentElement.classList.add("error");
-            }
-        });
-    }
-*/
-
+// execute this when user submits the form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   sendEmail();
+
+  setTimeout(() => {
+    form.reset();
+  }, 2500);
 });
