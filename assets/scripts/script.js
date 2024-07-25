94781bb4d5feac696ce2201e28657c9b22e91fe0
Saadaf Mohsin <155853706+SaadafAlt@users.noreply.github.com>
2024-07-24 21:36:37 -0400

    refactoring script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,22 +1,24 @@
 /**
  * sticky navbar
  */
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+// When the user scrolls the page, execute runSticky
+window.onscroll = function () {
+  runSticky();
+};
 
 var navbar = document.getElementById("nav-ul");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-/* Add the sticky class to the navbar when you reach its scroll position. 
-Remove "sticky" when you leave the scroll position */
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+// Add the sticky class to the navbar when you reach its scroll position.
+function runSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    // Remove "sticky" when you leave the scroll position
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
 /**
@@ -26,54 +28,52 @@ const emailElement = document.getElementById("my-email");
 const emailValue = emailElement.innerText;
 
 const copyContent = async () => {
-    try {
-        await navigator.clipboard.writeText(emailValue);
+  try {
+    await navigator.clipboard.writeText(emailValue);
 
-        emailElement.innerText = "Copied!";
-        setTimeout(() => {
-            emailElement.innerText = "saadafmohsin@gmail.com";
-        }, 1000);
+    emailElement.innerText = "Copied!";
+    setTimeout(() => {
+      emailElement.innerText = "saadafmohsin@gmail.com";
+    }, 1000);
 
-        console.log('Content copied to Clipboard');
-    } catch (err) {
-        console.error('Failed to copy: ', err);
-    }
-}
+    console.log("Content copied to Clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
 
 /**
  * Enable clients to send emails
  */
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 const fullName = document.getElementById("name");
 const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const mess = document.getElementById("message");
 
 function sendEmail() {
-    const bodyMessage = `
+  const bodyMessage = `
         Full Name: ${fullName.value}<br>
         Email: ${email.value}<br>
         Message: ${mess.value}
     `;
 
-    Email.send({
-        SecureToken: "857f1dc5-b045-4f53-9108-605fdf60f734",
-        To: 'saadafmohsin@gmail.com',
-        From: "saadafmohsin@gmail.com",
-        Subject: subject.value,
-        Body: bodyMessage
-        // Port: 2525
-    }).then(
-        message => {
-            if (message == "OK") {
-                Swal.fire({
-                    title: "Success!",
-                    text: "Message sent successfully.",
-                    icon: "success"
-                });
-            }
-        }
-    );
+  Email.send({
+    SecureToken: "857f1dc5-b045-4f53-9108-605fdf60f734",
+    To: "saadafmohsin@gmail.com",
+    From: "saadafmohsin@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+    // Port: 2525
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully.",
+        icon: "success",
+      });
+    }
+  });
 }
 
 /*
@@ -100,7 +100,7 @@ function sendEmail() {
 */
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    sendEmail();
-})
+  sendEmail();
+});
